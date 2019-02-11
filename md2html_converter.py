1aea52d38ae3cdc067f0f37311d1b21fce0a1a06
Gabriel F <43410045+GabrielFGRL@users.noreply.github.com>
2019-02-11 23:03:33 +0100

    Add files via upload

--- md2html_converter.py
+++ md2html_converter.py
@@ -1,6 +1,7 @@
 from markdown2 import Markdown
 import os
 import click
+import glob
 
 @click.command()
 @click.option("-i","--input-directory", "--input_directory",default='',help='dossier a convertir')
@@ -9,13 +10,19 @@ import click
 
 
 def convert(input_directory,output_directory,website_name):
+
         if os.path.exists(output_directory):
                 print("Impossible de créer le dossier : \nLe dossier '{}' existe déjà".format(output_directory))
 
         else:
+                mdintofolder = False
+                while mdintofolder == False:
+                        os.chdir(input_directory)
+                        mdintofolder = glob.glob('*.md')
+
                 os.mkdir(output_directory)
                 out_dir = open("./{}/{}.html".format(output_directory,website_name), "w")
-                with open(input_directory,"r") as fichier:
+                with open(mdintofolder[0],"r") as fichier:
                         out_dir.write('<!DOCTYPE html>\n<head>\n<meta charset="utf-8">\n</head>\n<body>\n')
                         out_dir.write(Markdown().convert(fichier.read()))
                         out_dir.write('\n</body>')
@@ -23,5 +30,3 @@ def convert(input_directory,output_directory,website_name):
 
 if __name__ == '__main__':
         convert()
-
-
